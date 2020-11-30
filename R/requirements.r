e3a4aeb21e79756a0e7bcf5a630897cec8bd187e
Hugh Garner <hugh.garner@gmail.com>
2020-11-30 10:26:51 +0000

    - updated ubuntu Dockerfile to use precompiled r binaries

--- R/requirements.r
+++ R/requirements.r
@@ -8,4 +8,7 @@ devtools::install_github('https://github.com/datashield/dsBaseClient', ref='v6.1
 # complains if not...
 devtools::install_github('https://github.com/datashield/DSLite')
 devtools::install_github('https://github.com/datashield/dsBase')
+
+# testthat 3.0.0 causes a bug where test_file can't seem to find 
+# the ds env, so fall back to 2.3.2
 install_version("testthat", version = "2.3.2", repos = "http://cran.us.r-project.org")
