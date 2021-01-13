f8405e24b0f38db29b494a2d6bc1e3a20d9e4008
Hugh Garner <hugh.garner@gmail.com>
2021-01-13 10:30:34 +0000

    - fixed path for CNSIM.zip

--- opal_import_cnsim.sh
+++ opal_import_cnsim.sh
@@ -16,7 +16,7 @@ sleep 5
 
 /usr/bin/opal project --verbose --opal http://opal:8080 -u administrator -p 'password' --add --name CNSIM --database mongodb
 
-/usr/bin/opal file --opal http://opal:8080 -u administrator -p 'password' --upload /datashield-loadtest_parent/CNSIM.zip /home/administrator
+/usr/bin/opal file --opal http://opal:8080 -u administrator -p 'password' --upload /datashield-loadtest/CNSIM.zip /home/administrator
 
 /usr/bin/opal import-xml --opal http://opal:8080 -u administrator -p 'password' --path /home/administrator/CNSIM.zip --destination CNSIM
 
